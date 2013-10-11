2650ced3038437019ed5d7df1d82d125b83bc66f
Rainer Keerdo <eestikaa@gmail.com>
2013-10-11 15:54:59 +0300

    Build xml, logging works.

--- POS/src/ee/ut/math/tvt/Labidas/Intro.java
+++ POS/src/ee/ut/math/tvt/Labidas/Intro.java
@@ -3,12 +3,13 @@ package ee.ut.math.tvt.Labidas;
 import javax.swing.JFrame;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class Intro {
 	private static org.apache.log4j.Logger logger = Logger.getLogger(Intro.class);
 
 	public static void main(String[] args) {
-		
+		PropertyConfigurator.configure("log4j.properties");
 		IntroUI mainWindow = new IntroUI();
 		logger.info("Window has been opened.");
 	}
