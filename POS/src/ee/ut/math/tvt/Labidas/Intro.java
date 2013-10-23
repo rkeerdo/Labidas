e217f64b6c3ab37b77b6a4aae73ff1abb7e211fe
Rainer Keerdo <r.keerdo@gmail.com>
2013-10-23 13:23:38 +0300

    Intro changed, logger works again. Ant update.

--- POS/src/ee/ut/math/tvt/Labidas/Intro.java
+++ POS/src/ee/ut/math/tvt/Labidas/Intro.java
@@ -5,14 +5,37 @@ import javax.swing.JFrame;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
+
 
 public class Intro {
-	private static org.apache.log4j.Logger logger = Logger.getLogger(Intro.class);
+	private static final org.apache.log4j.Logger logger = Logger.getLogger(Intro.class);
+	private static final String MODE = "console";
 
 	public static void main(String[] args) {
 		PropertyConfigurator.configure("log4j.properties");
-		IntroUI mainWindow = new IntroUI();
-		logger.info("Window has been opened.");
-	}
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
 
+		if (args.length == 1 && args[0].equals(MODE)) {
+			logger.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			IntroUI introUI = new IntroUI();
+
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
+
