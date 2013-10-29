9fe1b8520045d5fe7aff6a11ad341a55813a9355
Kantratas <eestikaa@gmail.com>
2013-10-29 21:45:11 +0200

    Homework 4 final commit.

--- POS/src/ee/ut/math/tvt/salessystem/utils/WarehouseStateException.java
+++ POS/src/ee/ut/math/tvt/salessystem/utils/WarehouseStateException.java
@@ -4,6 +4,7 @@ public class WarehouseStateException extends Exception{
 	public WarehouseStateException(){
 		super();
 	}
+	// Not sure where to use this or if it should be expanded.
 	public WarehouseStateException(String message){
 		super(message);
 	}
