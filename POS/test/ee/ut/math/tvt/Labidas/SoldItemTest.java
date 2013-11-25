c5fdd39f7304d25ca1639dcb291233e30b9566ac
Joosep Heinmets <joosephe@gmail.com>
2013-11-26 01:41:26 +0200

    SoldItemTest implemented

--- POS/test/ee/ut/math/tvt/Labidas/SoldItemTest.java
+++ POS/test/ee/ut/math/tvt/Labidas/SoldItemTest.java
@@ -1,24 +1,30 @@
 package ee.ut.math.tvt.Labidas;
 
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
 public class SoldItemTest {
 
+	SoldItem testItem;
+	SoldItem testItemEmpty;
 	@Before
 	public void setUp() {
-		
+		testItem = new SoldItem(new StockItem((long) 100, "test", "", 5.0), 5);
+		testItemEmpty = new SoldItem(new StockItem((long) 100, "test", "", 5.0), 0);
 	}
 	
 	@Test
 	public void testGetSum() {
-		//TODO
+		assertEquals(testItem.getSum(), 25.0, 0.001);
 	}
 	
 	@Test
 	public void testGetSumWithZeroQuantity() {
-		//TODO
+		assertEquals(testItemEmpty.getSum(), 0.0, 0.001);
 	}
 }
