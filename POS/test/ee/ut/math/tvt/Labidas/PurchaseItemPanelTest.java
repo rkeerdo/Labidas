540be51d44272790632418d74ebeda821337fde4
Kaspar Kesli <kaspar93@msn.com>
2013-11-27 16:28:14 +0200

    1 test done(PurchaseItemPanelTest).

--- POS/test/ee/ut/math/tvt/Labidas/PurchaseItemPanelTest.java
+++ POS/test/ee/ut/math/tvt/Labidas/PurchaseItemPanelTest.java
@@ -1,34 +1,68 @@
 package ee.ut.math.tvt.Labidas;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+import ee.ut.math.tvt.salessystem.ui.model.PurchaseInfoTableModel;
+import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
+import ee.ut.math.tvt.salessystem.ui.panels.PurchaseConfirmationPanel;
 import ee.ut.math.tvt.salessystem.ui.panels.PurchaseItemPanel;
 
 public class PurchaseItemPanelTest {
+	private PurchaseInfoTableModel model;
+	private SoldItem item1;
+	private SoldItem item2;
+	private PurchaseConfirmationPanel panel;
 	
 	@Before
 	public void setUp() {
-		//TODO
+		model= new PurchaseInfoTableModel();
+		item1=new SoldItem(new StockItem(new Long(1), "Soda","Beverage",1.0,10),3);
+		item2=new SoldItem(new StockItem(new Long(2), "Beer","Beverage",2.0,10),1);
 	}
-	
+
 	@Test
 	public void testAddSoldItem() {
-		//TODO
+		model.addItem(item1);
+		assertEquals(model.getTableRows().get(0).getPrice(),1.0,0.001);
 	}
 	
 	@Test
 	public void testGetSumWithNoItems() {
-		//TODO
+		double sum = 0;
+		List<SoldItem> soldItems = model.getTableRows();
+		for(SoldItem item : soldItems){
+			sum = sum + item.getSum();
+		}
+		assertEquals(sum,0.0,0.001);
 	}
 	
 	@Test
 	public void testGetSumWithOneItem() {
-		//TODO
+		double sum = 0;
+		model.addItem(item1);
+		List<SoldItem> soldItems = model.getTableRows();
+		for(SoldItem item : soldItems){
+			sum = sum + item.getSum();
+		}
+		assertEquals(sum,3.0,0.001);
 	}
 	
 	@Test
 	public void testGetSumWithMultipleItems() {
-		//TODO
+		double sum = 0;
+		model.addItem(item1);
+		model.addItem(item2);
+		List<SoldItem> soldItems = model.getTableRows();
+		for(SoldItem item : soldItems){
+			sum = sum + item.getSum();
+		}
+		assertEquals(sum,5.0,0.001);
 	}
 }
